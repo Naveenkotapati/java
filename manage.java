2b343b5089b542eb6f85c4d98fa871da3ea6274b
Naveenkotapati <113355206+Naveenkotapati@users.noreply.github.com>
2023-07-10 10:12:33 +0530

    Create manage.java

---
+++ manage.java
@@ -0,0 +1,12 @@
+class manage
+{
+public static void main(String[]args)
+{
+String studentname="naveen";
+int reg=192011356;
+String location="chennai";
+System.out.println("Student Name:"+studentname);
+System.out.println("Student Reg no. :"+reg);
+System.out.println("Student Located in:"+location);
+}
+}
